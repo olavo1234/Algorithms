1f9a23ce00c77e98cfff12f64abeebc752c70be4
olavodotpy <olavoazevedo88@gmail.com>
2024-09-06 00:27:42 -0300

    New site, refatoring

---
+++ HTML-CSS/basic-structure/script/script.js
@@ -0,0 +1,29 @@
+var contador = 0;
+
+const botaoModo = document.getElementById("alterarModo");
+
+botaoModo.addEventListener('click', function(){ 
+    if (contador % 2 == 0) {
+        const elementos = document.querySelectorAll("body");
+        elementos.forEach(elemento => {
+            elemento.style.background = "white";
+        });
+        
+        const font_elementos = document.querySelectorAll(".steinsgate");
+        font_elementos.forEach(font_elementos => {
+            font_elementos.style.color = 'black'
+        });
+    }else{
+        const elementos = document.querySelectorAll("body");
+        elementos.forEach(elemento => {
+            elemento.style.background = "black";
+        });
+        
+        const font_elementos = document.querySelectorAll(".steinsgate");
+        font_elementos.forEach(font_elementos => {
+            font_elementos.style.color = 'white'
+        });
+    }; 
+    
+    contador++;
+});
